41dc2da668e64a4b8b684dc6a168143b63cda383
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-10-31 15:55:58 +0600

    add function for grade add

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ class Person:
         self.address: str = address
 
     def display_person_info(self):
-        print(f"Name: {self.name}, Age: {self.age}, Address: {self.address}")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Address: {self.address}")
 
 
 class Student(Person):
@@ -22,7 +24,8 @@ class Student(Person):
         super().__init__(name, age, address)
         self.student_id: str = student_id
         self.grades: Dict = {}
-        self.courses: List['Course'] = []
+        self.courses: List["Course"] = []
+        print(f"Student {name} (ID: {student_id}) is added successfully.")
 
     def add_grade(self, course_code: str, grade: str):
         is_enrolled = [
@@ -36,28 +39,28 @@ class Student(Person):
             print(f"Error: {self.name} is not enrolled in {course_code} course.")
 
     def enroll_course(self, course: "Course"):
-        
+
         if course not in self.courses:
             self.courses.append(course)
             print(
-                f"Congratulations. {self.name} is successfully enrolled in {course.course_name} course."
+                f"Student {self.name} (ID: {self.student_id}) is enrolled in {course.course_name} (Code: {course.course_code})."
             )
         else:
-            print(f"{self.name} is already enrolled in {course.course_name} course")
+            print(
+                f"Student {self.name} (ID: {self.student_id}) is already enrolled in {course.course_name} (Code: {course.course_code})."
+            )
 
     def display_student_info(self):
+        print("Student Information:")
         self.display_person_info()
         print(f"ID: {self.student_id} ")
 
-        # show grades
-        print("Grades: { ")
-        for x, y in self.grades.items():
-            print(f"Course: {x}: {y}")
-        print(" }")
-
         # show courses
-        print("Enrolled Courses:")
-        print(f"{", ".join([course.course_name for course in self.courses])}")
+        print(
+            f"Enrolled Courses: {", ".join([course.course_name for course in self.courses])}"
+        )
+        # show grades
+        print(f"Grades: {self.grades}")
 
 
 class Course:
@@ -66,12 +69,17 @@ class Course:
         self.course_code = course_code
         self.instructor = instructor
         self.students = []
+        print(
+            f"Course {course_name} (Code: {course_code}) is created with instructor {instructor}."
+        )
 
     def add_student(self, student):
 
         if student not in self.students:
             self.students.append(student)
-            print(f"{student.name} is succesfully enrolled in {self.course_name} course")
+            print(
+                f"{student.name} is succesfully enrolled in {self.course_name} course"
+            )
         else:
             print(f"{student.name} is already enrolled in {self.course_name} course")
 
@@ -81,28 +89,54 @@ class Course:
         print(f"Code: {self.course_code}")
         print(f"Instructor: {self.instructor}")
         print(
-            "Enrolled Students:",
+            "Enrolled Students: ",
             ", ".join([student.name for student in self.students])
             or "No students enrolled",
         )
 
 
-math_course = Course("Math", "MATH101", "Azam")
-physics_course = Course("Physics", "PHY101", "Esha")
+class StudentManagementSystem:
+    def __init__(self):
+        self.students: Dict[str, "Student"] = {}
+        self.courses: Dict[str, "Course"] = {}
 
-student1 = Student("Irham", 5, "cumilla", "0")
-student2 = Student("Irham2", 6, "cumilla", "1")
-student1.enroll_course(math_course)
-student1.enroll_course(math_course)
+    def add_new_student(self, name, age, address, student_id):
+        if student_id not in self.students:
+            new_student = Student(name, age, address, student_id)
+            self.students[student_id] = new_student
+        else:
+            print(f"Error: Student: {name} (ID: {student_id}) already exists")
 
-# student1.add_grade("MATH101", "A")
+    def add_new_course(self, course_name, course_code, instructor):
+        if course_code not in self.courses:
+            new_course = Course(course_name, course_code, instructor)
+            self.courses[course_code] = new_course
+        else:
+            print(f"Error: Course {course_name} (Code: {course_code}) already exists.")
 
+    def enroll_student_in_course(self, student_id, course_code):
+        student = self.students.get(student_id)
+        course = self.courses.get(course_code)
+        
+        if student and course :
+            student.enroll_course(course)
+        else:
+            print("Error: Invalid student ID or course code.")
 
-# student1.enroll_course(math_course)
+    def add_grade_for_student(self, student_id, course_code, grade):
+        student = self.students.get(student_id)
+        course = self.courses.get(course_code)
+        
+        if student and course :
+            student.add_grade(course_code,grade)
+        else:
+            print("Error: Invalid student ID or course code.")
+    
+    
+sms = StudentManagementSystem()
 
-math_course.add_student(student1)
-math_course.add_student(student2)
-math_course.add_student(student2)
-math_course.display_course_info()
-# math_course.add_student(student2)
-# math_course.add_student(student2)
+sms.add_new_student("azam", 28, "abc", "0")
+sms.add_new_course("Math", "Math101", "abc")
+
+sms.enroll_student_in_course("0", "Math101")
+
